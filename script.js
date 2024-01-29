6f6f1dafe040a13868f9ee4b23e9c71159d18a5d
saraali125 <157318423+saraali125@users.noreply.github.com>
2024-01-29 02:57:08 +0000

    green box var

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 function setup() {
   createCanvas(400, 300); // creates a drawing canvas
 }
-var y = 220; 
+var apple = 220; 
 function draw() {
   // background is drawn first
   background(240); // light gray
 
   // call a simple P5 drawing function, such as ellipse() using your new variable(s) in place of arguments
   fill(0, 255, 0)
-  rect(y, 40, 100, 100);
+  rect(apple, 40, 100, 100);
 
 
 
